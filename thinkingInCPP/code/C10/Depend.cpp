4723daecf0019713e6730bb0f638cf3a12548aa3
Walter Zhao <walther.zhao@gmail.com>
2016-11-06 15:35:15 +0800

    update book source code

--- thinkingInCPP/code/C10/Depend.cpp
+++
@@ -1,8 +0,0 @@
-//: C10:Depend.cpp {O}
-// From Thinking in C++, 2nd Edition
-// Available at http://www.BruceEckel.com
-// (c) Bruce Eckel 1999
-// Copyright notice in Copyright.txt
-// Static initialization
-#include "Depend.h"
-///:~
